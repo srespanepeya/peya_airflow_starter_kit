92cb0e63e3c2a7fe7b383ea8341b5ba6d9a2e91d
Diego Pietruszka <diego.pietruszka@peya.local>
2019-07-12 11:19:33 -0300

    primer commit talon proyect

---
+++ airflow/dags/talon_DAG.py
@@ -0,0 +1,49 @@
+from builtins import range
+from datetime import datetime, timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.slack_operator import SlackAPIPostOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+from airflow.contrib.operators.ssh_operator import SSHOperator
+
+# Params DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 7, 9),
+    'email': ['bigdata@pedidosya.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 15,
+    'retry_delay': timedelta(minutes=60)
+}
+
+# Funciones
+def validate():
+    #  ---- Logica validacion ----
+    # Generacion archivo
+    # Delta > x 
+    print('OK!')
+
+with DAG('Talon_DAG', schedule_interval='00 12 * * *', catchup=False, default_args=default_args) as dag:
+    # Extraccion de datos desde servicio talon
+    getDataTalonService = SSHOperator(
+        task_id="getDataTalonService",
+        ssh_conn_id='ssh_hadoop_datanode1',
+        bash_command="""
+        echo 'test'
+        """,
+        dag=dag)
+    )
+    # Mensaje OK
+    validate = PythonOperator(
+        task_id = messageOK,
+        python_callable = validate
+    )
+
+    getDataTalonService >> validate
+
