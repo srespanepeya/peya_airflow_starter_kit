ea492b54ea9a9cba381cc4f51cb04ffff0aca328
Diego Pietruszka <diego.pietruszka@peya.local>
2019-07-12 14:42:43 -0300

    primera prueba ssh servicio talon

--- airflow/dags/talon_DAG.py
+++ airflow/dags/talon_DAG.py
@@ -38,7 +38,7 @@ with DAG('Talon_DAG', schedule_interval='00 12 * * *', catchup=False, default_ar
     getDataTalonService = SSHOperator(
         task_id="getDataTalonService",
         command="""
-        /home/hduser/backendbi-procesos/start_backendbi-procesos_weekly.sh
+        ls /home/hduser/backendbi-procesos/
         """,
         timeout = 20,
         ssh_conn_id = "ssh_hadoop_datanode1_ti"
