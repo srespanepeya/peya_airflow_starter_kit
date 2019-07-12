07ce6c68bd3a10b26c589b1d425e90fd4e1d4476
Diego Pietruszka <diego.pietruszka@peya.local>
2019-07-12 12:10:08 -0300

    primera prueba ssh servicio talon

--- airflow/dags/talon_DAG.py
+++ airflow/dags/talon_DAG.py
@@ -46,7 +46,7 @@ with DAG('Talon_DAG', schedule_interval='00 12 * * *', catchup=False, default_ar
 
     # Mensaje OK
     validate = PythonOperator(
-        task_id = validate,
+        task_id = "validate",
         python_callable = validate_message
     )
 
