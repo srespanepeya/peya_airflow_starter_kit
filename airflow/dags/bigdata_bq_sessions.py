3ce655b818902a8f71eea3665108e6edf6c88170
Diego Pietruszka <diego.pietruszka@peya.local>
2019-07-29 11:48:44 -0300

    DAG bigquery sessions to hdfs

--- airflow/dags/bigdata_bq_sessions.py
+++ airflow/dags/bigdata_bq_sessions.py
@@ -19,7 +19,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2019, 6, 27),
-    'email': ['diego.pietruszka@pedidosya.com'],
+    'email': ['bigdata@pedidosya.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 15,
@@ -32,7 +32,7 @@ table_name_ios = '`110191523.ga_sessions_' + yesterday_date + '`'
 table_name_webd = '`110187139.ga_sessions_' + yesterday_date + '`'
 table_name_webm = '`110187640.ga_sessions_' + yesterday_date + '`'
 
-with DAG('BigData_BQSessions', schedule_interval='00 10 * * *', catchup=False, default_args=default_args) as dag:
+with DAG('BigData_BQSessions', schedule_interval='0 9 * * *', catchup=False, default_args=default_args) as dag:
 
     execution_start = DummyOperator(
         task_id='execution_start',
