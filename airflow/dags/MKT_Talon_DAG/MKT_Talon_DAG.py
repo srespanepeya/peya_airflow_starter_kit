e3ed3ba680e1e7cbaa2407343ff316b12ef57ba1
Diego Pietruszka <diego.pietruszka@peya.local>
2019-08-06 12:40:02 -0300

    validation_hdfs_to_s3

--- airflow/dags/MKT_Talon_DAG/MKT_Talon_DAG.py
+++ airflow/dags/MKT_Talon_DAG/MKT_Talon_DAG.py
@@ -147,6 +147,6 @@ with DAG('MKT_Talon_DAG', schedule_interval=None, catchup=False, default_args=de
     # usuario de amazon para Santiago y Nicolas
     # Columnas del ODS, de que archivos vienen... 
 
-    get_data_from_talon_service >> [copy_data_from_lfs_to_hdfs_campaigns,copy_data_from_lfs_to_hdfs_coupons] \ 
-        >> check_point_1 >> [process_data_and_move_to_s3_campaigns,process_data_and_move_to_s3_coupons] >> check_point_2 \ 
-            >> [dwh_load_coupons_from_s3,dwh_load_campaigns_from_s3] >> validation_dwh_load_coupons_and_campaigns_from_s3 >> dwh_generate_fact_talon_coupons
+    get_data_from_talon_service >> [copy_data_from_lfs_to_hdfs_campaigns,copy_data_from_lfs_to_hdfs_coupons] >> check_point_1 
+        check_point_1 >> [process_data_and_move_to_s3_campaigns,process_data_and_move_to_s3_coupons] >> check_point_2
+            check_point_2 >> [dwh_load_coupons_from_s3,dwh_load_campaigns_from_s3] >> validation_dwh_load_coupons_and_campaigns_from_s3 >> dwh_generate_fact_talon_coupons
