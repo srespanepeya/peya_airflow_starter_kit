9dc2c15484c0e462fd9ecc01b5e0c60597c46630
Santiago Respane <srespane@gmail.com>
2019-08-19 11:27:51 -0300

    deleted mkt talon dag

--- airflow/dags/MKT_Talon_DAG/sh/audit_talon_s3_to_imports_ods_redshift.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "COMIENZO AUDITORIA TALON LOCAL FILE SYSTEM TO HFDS"
-/home/hduser/spark/bin/spark-submit --master local[4] --driver-memory 7G --driver-cores 4 --executor-memory 7G --conf spark.cores.max=4 /home/hduser/airflow-scripts/audit_talon_s3_coupons_to_imports_ods_redshift.py -e $1
