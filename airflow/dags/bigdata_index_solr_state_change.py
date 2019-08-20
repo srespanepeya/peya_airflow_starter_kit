b754189b3707f4f7f133be0329acbeddc559ce57
carlos-cristoforone <ccristoforone@gmail.com>
2019-08-20 13:13:08 -0300

    agregado heart_beat y state_change

---
+++ airflow/dags/bigdata_index_solr_state_change.py
@@ -0,0 +1,39 @@
+from builtins import range
+from datetime import datetime, timedelta, date
+import os
+import stat
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.slack_operator import SlackAPIPostOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks import SSHHook
+
+# Variables
+
+# Params DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 7, 9),
+    'email': ['diego.pietruszka@pedidosya.com','carlos.cristoforone@pedidosya.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(seconds=5)
+}
+
+with DAG('BigData_Writer_State_Change_Solr_DAG', schedule_interval="*/2 * * * 1-7", catchup=False, default_args=default_args) as dag:
+    write_index_solr_state_change = SSHOperator(
+        task_id="write_index_solr_state_change",
+        command="""
+        /usr/bin/bash /home/hduser/backendbi-procesos/BigDataReceptionEventToSolr/STATE_CHANGE.sh
+        """,
+        timeout = 1600,
+        ssh_conn_id = "ssh_hadoop_datanode1_ti"
+    )
+
+write_index_solr_state_change   
