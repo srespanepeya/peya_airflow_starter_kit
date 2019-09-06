80654b20ac10e9b37ebd35d763586926e6b8441f
carlos-cristoforone <ccristoforone@gmail.com>
2019-09-06 16:12:54 -0300

    flow session hdfs to ods

---
+++ airflow/dags/bigdata_create_session_partition.py
@@ -0,0 +1,66 @@
+from builtins import range
+from datetime import datetime, timedelta, date
+import os
+import stat
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.slack_operator import SlackAPIPostOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks import SSHHook
+
+# Variables
+
+#Dummy operator
+from airflow.operators.dummy_operator import DummyOperator
+
+# Params DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'wait_for_downstream': True,
+    'start_date': datetime(2019, 7, 9),
+    'email': ['diego.pietruszka@pedidosya.com','carlos.cristoforone@pedidosya.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(seconds=5)
+}
+
+with DAG('BigData_Flow_Session_Related_Hdfs_To_Ods', schedule_interval="0 */1 * * 1-7", catchup=False, default_args=default_args) as dag:
+    
+    fs = SSHOperator(
+        task_id="flow_session_hdfs_to_ods",
+        command="""
+        /usr/bin/bash /home/hduser/hive/scripts/create_table_alan_flow_sessions_peya.sh
+        """,
+        timeout = 60,
+        ssh_conn_id = "ssh_hadoop_resmanager_bi",
+        dag = dag
+    )
+
+    fse = SSHOperator(
+        task_id="flow_session_event_hdfs_to_ods",
+        command="""
+        /usr/bin/bash /home/hduser/hive/scripts/create_table_alan_flow_sessions_event_peya.sh
+        """,
+        timeout = 60,
+        ssh_conn_id = "ssh_hadoop_resmanager_bi",
+        dag = dag
+    )
+
+    fsc = SSHOperator(
+        task_id="flow_session_chat_hdfs_to_ods",
+        command="""
+        /usr/bin/bash /home/hduser/hive/scripts/create_table_alan_flow_sessions_chats.sh
+        """,
+        timeout = 60,
+        ssh_conn_id = "ssh_hadoop_resmanager_bi",
+        dag = dag
+    )
+
+fs >> fse >> fsc
+   
