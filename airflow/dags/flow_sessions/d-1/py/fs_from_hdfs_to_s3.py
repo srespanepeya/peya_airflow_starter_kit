6137f285603f8634867a9bc4cbdf364e63518a8d
carlos-cristoforone <ccristoforone@gmail.com>
2019-09-13 09:43:55 -0300

    actualizacion de py de copiado a s3

--- airflow/dags/flow_sessions/d-1/py/fs_from_hdfs_to_s3.py
+++ airflow/dags/flow_sessions/d-1/py/fs_from_hdfs_to_s3.py
@@ -1,8 +1,3 @@
-# Se importan las librerias argparse y ConfigParser para recibir parametros en las funciones
-import argparse, ConfigParser
-# se importan las librerias os y sys para enviar comandos al sistema operativo.
-import os, sys
-# se importa la libreria traceback para manejo de errores y excepciones
 import traceback
 # se importa la libreria time para realiza un sleep mientras que spark finaliza la consolidacion de los archivos
 import time
@@ -32,14 +27,10 @@ def move_data(app_args):
         sc = SparkContext(conf=conf)
         sqlContext = SQLContext(sc)
         print('--->START READING FS DATA FROM HDFS')
-        df1 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_sessions/{0}/*.parquet'.format(partition))
-        df2 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_session_events/{0}/*.parquet'.format(partition))
-        df3 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_sessions_chat/{0}/*.parquet'.format(partition))
-        print('<---END READING COUPONS DATA FROM HDFS')
-        print('----@>ROW COUNT:{0}'.format(df.count()))
-        df.printSchema()
-        print('<---END DISCOVERING AND ADJUSTING SCHEMA')
-        
+        df1 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_sessions/{0}/*'.format(partition))
+        df2 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_session_events/{0}/*'.format(partition))
+        df3 = sqlContext.read.parquet('hdfs://hadoop-namenode-bi:9000/alan/ods/flow_sessions_chats/{0}/*'.format(partition))
+
         print('--->START WRITING DATA ON S3')
         df1.write.mode ("overwrite") \
                 .format("com.databricks.spark.csv") \
@@ -55,15 +46,13 @@ def move_data(app_args):
                 .format("com.databricks.spark.csv") \
                 .option("encoding", "UTF-8") \
                 .option("codec", "org.apache.hadoop.io.compress.GzipCodec") \
-                .save("s3a://peyabi.bigdata/flow_sessions/flow_sessions_chat/{0}".format(partition))
-                
+                .save("s3a://peyabi.bigdata/flow_sessions/flow_sessions_chats/{0}".format(partition))
+
         print('<---END WRITING DATA ON S3')
     except:
         print(traceback.format_exc())
         time.sleep(1) #workaround para el bug del thread shutdown
         raise
-        
-        
 
 def get_app_args():
         parser = argparse.ArgumentParser()
