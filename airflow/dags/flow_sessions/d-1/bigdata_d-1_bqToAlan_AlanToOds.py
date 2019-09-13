e71ab9dc198840ce7ccc9b8c6f35bf5b3e4a602e
carlos-cristoforone <40300373+carlos-cristoforone@users.noreply.github.com>
2019-09-12 22:18:04 -0300

    Update bigdata_d-1_bqToAlan_AlanToOds.py

--- airflow/dags/flow_sessions/d-1/bigdata_d-1_bqToAlan_AlanToOds.py
+++ airflow/dags/flow_sessions/d-1/bigdata_d-1_bqToAlan_AlanToOds.py
@@ -109,8 +109,7 @@ with DAG('BigData_Flow_Session_Related_Hdfs_to_S3', schedule_interval="0 7 * * 1
     )
 
     dummy = DummyOperator(task_id='dummy', dag=dag)
-    join = DummyOperator(task_id='join', dag=dag)
-    
+   
     s3 = BashOperator(
         task_id='process_data_and_move_to_s3_fs',
         trigger_rule='none_failed',
